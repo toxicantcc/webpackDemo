48eb651bee7953c8601fa49be0f7dc60920dce74
toxicantcc <1263936713@qq.com>
2020-05-26 14:58:22 +0800

    [webpackdemo-管理资源]

--- README.md
+++ README.md
@@ -9,6 +9,16 @@ npm install --save-dev webpack-cli
 <!-- 全局安装 -->
 npm install --global webpack
 ```
+### 管理资源
++ css
+  ```
+  <!-- 安装css-loader 和 style-loader 识别css文件-->
+  npm install --save-dev css-loader style-loader
+  <!-- 安装 file-loader 识别图片 -->
+  npm install --save-dev file-loader
+  <!-- 安装 csv-loader xml-loader 支持CSV TSV XML文件，JSON文件内置支持 -->
+  npm install --save-dev csv-loader xml-loader
+  ```
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import _ from 'lodash'
+import './index.css'
+import img from './cat.jpg'
+import Data from './data.xml'
 
 function component() {
   var element = document.createElement('div');
   
   element.innerHTML = _.join(['Hello', 'Webpack'], '');
+  // 添加css
+  element.classList.add('head');
+
+  // 添加图像
+  var myImg = new Image();
+  myImg.src = img;
+  element.appendChild(myImg);
+
+  // 控制栏查看数据
+  console.log(Data);
 
   return element;
 }
